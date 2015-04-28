6ceea4b4370801eb2cce00c246cafad465c37ae9
zhaieiMac <zhailei2011@gmail.com>
2015-04-28 22:07:52 +0800

    change router

--- public/app/app.js
+++ public/app/app.js
@@ -2,7 +2,7 @@ angular.module('userApp', ['app.routes', 'authService', 'mainCtrl', 'userCtrl', 
 
 // application configuration to integrate token into requests
 .config(function($httpProvider) {
-
+	
 	// attach our auth interceptor to the http requests
 	$httpProvider.interceptors.push('AuthInterceptor');
 
