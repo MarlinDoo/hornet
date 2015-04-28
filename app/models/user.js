d43bb53e55302b7462d7ae233f1dec38943e9c3c
zhaieiMac <zhailei2011@gmail.com>
2015-04-28 22:07:15 +0800

    add key words

--- app/models/user.js
+++ app/models/user.js
@@ -5,32 +5,27 @@ var bcrypt 		 = require('bcrypt-nodejs');
 // user schema 
 var UserSchema   = new Schema({
 	name: String,
+	sex:String,
 	username: { type: String, required: true, index: { unique: true }},
 	password: { type: String, required: true, select: false },
-	phone:Number
+	phone:Number,
+	summary:String,
+	address:String
 });
 
 // hash the password before the user is saved
 UserSchema.pre('save', function(next) {
 	var user = this;
-
-	// hash the password only if the password has been changed or user is new
 	if (!user.isModified('password')) return next();
-
-	// generate the hash
 	bcrypt.hash(user.password, null, null, function(err, hash) {
 		if (err) return next(err);
-
-		// change the password to the hashed version
 		user.password = hash;
 		next();
 	});
 });
 
-// method to compare a given password with the database hash
 UserSchema.methods.comparePassword = function(password) {
 	var user = this;
-
 	return bcrypt.compareSync(password, user.password);
 };
 
