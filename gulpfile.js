9dfea8929a4c2ed58eb2c101bacf1efac8241784
zhaieiMac <zhailei2011@gmail.com>
2015-05-21 15:48:52 +0800

    test Gmail send email

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,9 @@ var nodemon   = require('gulp-nodemon');
 
 var BROWSER_SYNC_RELOAD_DELAY = 500;
 var browserSync = require('browser-sync');
-
 var reload = browserSync.reload;
+
+var nodemailer = require('nodemailer');
 
 gulp.task('css', function() {
   return gulp.src('public/assets/css/style.less')
@@ -37,6 +38,46 @@ gulp.task('watch', function() {
   // watch js files and run lint and run js and angular tasks
   gulp.watch(['server.js', 'public/app/*.js', 'public/app/**/*.js'], ['js']);
 });
+
+
+
+// var transporter = nodemailer.createTransport({
+//   service: 'Gmail',
+//   secureConnection: true,
+//   port: 465,
+//   auth: {
+//       user: 'zhailei2011@gmail.com',
+//       pass: 'zhailei1230321'
+//   }
+// });
+// 
+var transporter = nodemailer.createTransport();
+gulp.task('email',function(){
+  // transporter.sendMail({
+  //   from: 'zhailei2011',
+  //   to: 'csq0808@163.com',
+  //   subject: '邀请您加入通讯录',
+  //   text: 'text'
+  // },function(error,info){
+  //   if(error){
+  //    console.log(error);
+  //   }else{
+  //     console.log({message:info});
+  //   }
+  // });
+  transporter.sendMail({
+    from: 'zhailei2011@gmail.com',
+    to: '31665431@qq.com',
+    subject: '邀请您加入通讯录',
+    text: 'text'
+  },function(error,info){
+    if(error){
+     console.log(error);
+    }else{
+      console.log({message:info});
+    }
+  });
+})
 
 // the nodemon task
 var called = false;
